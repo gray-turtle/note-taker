6254f5ad816e7e25c0463b1407e7031fbcc692d5
Keith Fike <fkeithr@gmail.com>
2020-07-19 22:10:53 -0600

    added post requests, updated database

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const { notes } = require('./db/db.json');
 const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.port || 3002;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+
+  if (query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  if (query.text) {
+    filteredResults = filteredResults.filter(note => note.text === query.text);
+  }
+
+  return filteredResults;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
+}
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+
+  return note;
+}
 
 app.get('/db/db.json', (req, res) => {
   let results = notes;
@@ -21,6 +61,25 @@ app.get('/db/db.json/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.post('/db/db.json', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
